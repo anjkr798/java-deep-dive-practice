2c04331ff461c3c61f73a4e5aaaf7e269cfc53d3
Anjan Kumar Das <anjkr798>
2026-03-01 14:42:48 +0530

    stream api basics

--- stream-api-basics/src/test/java/com/anjan/labs/groupby/counting/StreamApiCountingTest.java
+++ stream-api-basics/src/test/java/com/anjan/labs/groupby/counting/StreamApiCountingTest.java
@@ -1,19 +1,20 @@
 package com.anjan.labs.groupby.counting;
 
+import com.anjan.labs.groupby.counting.dao.Employee;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 public class StreamApiCountingTest {
 
-    private final static Logger log = LoggerFactory.getLogger(StreamApiCountingTest.class);
 
     @Test
     public void basicFrequencyCountTest(){
+        log.info(" ======== basicFrequencyCountTest :: started ");
 
         List<String> listOfWords = Arrays.asList("apple", "banana", "apple", "cherry", "banana", "apple");
 
@@ -23,7 +24,26 @@ public class StreamApiCountingTest {
 
         Map<String,Long> resultMap = streamApiCounting.basicFrequencyCount(listOfWords);
 
-        log.info(" ======== basicFrequencyCountTest :: resultMap = {} ", resultMap);
+        log.info(" ======== basicFrequencyCountTest :: completed ");
 
+    }
+
+    @Test
+    public void groupObjectByPropertyTest(){
+
+        log.info(" ======== groupObjectByPropertyTest :: started ");
+
+        List<Employee> employees = Arrays.asList(
+                new Employee(101, "Alice", "HR", "10000"),
+                new Employee(102, "Bob", "IT", "20000"),
+                new Employee(103, "Charlie", "IT", "15000"),
+                new Employee(104, "David", "Finance", "25000"),
+                new Employee(105, "Eve", "HR", "20000")
+        );
+        StreamApiCounting streamApiCounting = new StreamApiCounting();
+
+        streamApiCounting.groupObjectByProperty(employees);
+
+        log.info(" ======== groupObjectByPropertyTest :: completed ");
     }
 }
